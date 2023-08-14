ae6a4198ef42c62dd330936e4016251189867f41
Rajat Nai <rajatnai49@gmail.com>
2023-08-14 12:08:09 +0530

    Change File strcture

---
+++ README.md
@@ -0,0 +1,62 @@
+# DevChatter
+
+<div align="center">
+
+<h3>🧑🏽‍💻 About</h3>
+
+<p>
+  DevChatter is a web-based social networking platform designed to connect
+  developers worldwide, enabling them to share knowledge, collaborate, and
+  engage in various learning challenges. The platform fosters a community of
+  developers to exchange ideas, seek assistance, and grow their technical skills
+  through meaningful interactions and real-time discussions.
+</p>
+</div>
+
+<div align="center">
+<h4>Steps to run projct</h4>
+</div>
+<ol>
+  <li>
+    <h6>Start backend node server</h6>
+    <ol>
+      <li>
+        <code><b>cd backend</b></code>
+      </li>
+      <li>
+        <code><b>npm install</b></code>
+      </li>
+      <li>
+        <code><b>npm start</b></code>
+      </li>
+    </ol>
+  </li>
+  <li>
+    <h6>Start frontend react server</h6>
+    <ol>
+      <li>
+        <code><b>cd frontend</b></code>
+      </li>
+      <li>
+        <code><b>npm install</b></code>
+      </li>
+      <li>
+        <code><b>npm run dev</b></code>
+      </li>
+      <li>
+        <code><b>URL: localhost:5173</b></code>
+      </li>
+    </ol>
+  </li>
+</ol>
+
+<div align="center">
+<h4>Admin</h4>
+</div>
+
+<a href="https://github.com/rajatnai49">
+    <img src="https://avatars.githubusercontent.com/u/96529504?v=4" style="height:50px; border:2px solid orange; border-radius:50px"/>
+</a>
+<a href="https://github.com/Mandar007007">
+    <img src="https://avatars.githubusercontent.com/u/90078170?v=4" style="height:50px; border:2px solid orange; border-radius:50px"/>
+</a>
--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,269 +1,256 @@
-const { post } = require("../app")
 const Post = require("../models/Post")
 const User = require("../models/User")
 
-exports.createPost = async(req,res) => {
-    try{
+exports.createPost = async (req, res) => {
+  try {
 
-        const newPostData = {
-            caption:req.body.caption,
-            image:{
-                public_id:"sample id",
-                url:"sample url"
-            },
-            owner:req.user._id
-        }
-
-        const post = await Post.create(newPostData)
-        const user = await User.findById(req.user._id)
-
-        user.posts.push(post._id)
-
-        await user.save()
-
-
-        res.status(201).json({
-            success:true,
-            post
-        })
-
-    }catch(e){
-        res.status(500).json({
-            success:false,
-            error:e.message
-        })
+    const newPostData = {
+      caption: req.body.caption,
+      image: {
+        public_id: "sample id",
+        url: "sample url"
+      },
+      owner: req.user._id
     }
+
+    const post = await Post.create(newPostData)
+    const user = await User.findById(req.user._id)
+
+    user.posts.push(post._id)
+
+    await user.save()
+
+
+    res.status(201).json({
+      success: true,
+      post
+    })
+
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: e.message
+    })
+  }
 
 }
 
-exports.deletePost = async (req,res,next) => {
-  try{
+exports.deletePost = async (req, res, next) => {
+  try {
 
     const post = await Post.findById(req.params.id)
-    
-    if(!post)
-    {
-      return res.status(404).json({success:false,message:"Post Not Found"})
+
+    if (!post) {
+      return res.status(404).json({ success: false, message: "Post Not Found" })
     }
 
 
-    if(post.owner.toString() !== req.user._id.toString())
-    {
-      return res.status(401).json({success:false,message:"Unauthorized"})
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ success: false, message: "Unauthorized" })
 
     }
 
-    await post.deleteOne({_id:req.params.id})
+    await post.deleteOne({ _id: req.params.id })
 
     const user = await User.findById(req.user._id)
 
     const index = user.posts.indexOf(req.params.id)
 
-    user.posts.splice(index,1);
+    user.posts.splice(index, 1);
 
     await user.save();
 
     res.status(200).json({
-      success:true,
-      message:"Post Deleted"
+      success: true,
+      message: "Post Deleted"
     })
 
-  }catch(e)
-  {
-    res.status(500).json({success:false,message:e.message})
+  } catch (e) {
+    res.status(500).json({ success: false, message: e.message })
   }
 }
 
 exports.likeAndUnlikePost = async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-  
-      if (!post) return res.status(404).json({ message: "Post Not Found" });
-  
-      const userId = req.user._id.toString();
-      const likes = post.likes.map(like => like._id.toString());
-  
-      if (likes.includes(userId)) {
-        const index = likes.indexOf(userId);
-        post.likes.splice(index, 1);
-        await post.save();
-        return res.status(200).json({ success: true, message: "Post Unliked" });
-      } else {
-        post.likes.push({ _id: req.user._id });
-        await post.save();
-        return res.status(200).json({ success: true, message: "Post Liked" });
-      }
-    } catch (e) {
-      res.status(500).json({ success: false, message: e.message });
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ message: "Post Not Found" });
+
+    const userId = req.user._id.toString();
+    const likes = post.likes.map(like => like._id.toString());
+
+    if (likes.includes(userId)) {
+      const index = likes.indexOf(userId);
+      post.likes.splice(index, 1);
+      await post.save();
+      return res.status(200).json({ success: true, message: "Post Unliked" });
+    } else {
+      post.likes.push({ _id: req.user._id });
+      await post.save();
+      return res.status(200).json({ success: true, message: "Post Liked" });
     }
-  };
-  
-
-  exports.getPostOfFollowing = async (req,res,next) => {
-    try{
-      const user = await User.findById(req.user._id).populate("following","posts");
-      const posts = await Post.find({
-        owner:{
-          $in : user.following
-        }
-      })
-
-      res.status(200).json({
-        success:true,
-        posts
-      })
-
-    }catch(e)
-    {
-      res.status(500).json({
-        success:false,
-        message:e.message
-      })
-    }
+  } catch (e) {
+    res.status(500).json({ success: false, message: e.message });
   }
+};
 
-  exports.updateCaption = async (req,res) => {
-    try{
 
-      const post = await Post.findById(req.params.id);
-
-      if(!post) return res.status(400).json({
-        success:false,
-        message:"Post Not Found"
-      })
-
-      if(post.owner.toString() !== req.user._id.toString())
-      {
-        res.status(400).json({
-          success:false,
-          message:"Unauthorized"
-        })
+exports.getPostOfFollowing = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).populate("following", "posts");
+    const posts = await Post.find({
+      owner: {
+        $in: user.following
       }
+    })
 
-      post.caption = req.body.caption
-      await post.save()
+    res.status(200).json({
+      success: true,
+      posts
+    })
 
-      res.status(200).json({
-        success:true,
-        message:"Post Updated"
-      })
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e.message
+    })
+  }
+}
 
-    }catch(e)
-    {
-      res.status(500).json({
-        success:false,
-        error:e.message
+exports.updateCaption = async (req, res) => {
+  try {
+
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(400).json({
+      success: false,
+      message: "Post Not Found"
+    })
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      res.status(400).json({
+        success: false,
+        message: "Unauthorized"
       })
     }
+
+    post.caption = req.body.caption
+    await post.save()
+
+    res.status(200).json({
+      success: true,
+      message: "Post Updated"
+    })
+
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: e.message
+    })
   }
+}
 
-  exports.commentOnPost = async (req,res,next) => {
-    try{
+exports.commentOnPost = async (req, res, next) => {
+  try {
 
-      const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id)
 
-      if(!post) res.status(404).json({
-        success:false,
-        message:"No posts found"
-      })
+    if (!post) res.status(404).json({
+      success: false,
+      message: "No posts found"
+    })
 
-      let commentIndex = -1
+    let commentIndex = -1
 
-      post.comments.forEach((item,index) => {
-          if(item.user.toString() === req.user._id.toString())
-          {
-            commentIndex = index;
-          }
-      })
-
-      if(commentIndex !== -1)
-      {
-        post.comments[commentIndex].comment = req.body.comment
-
-        await post.save();
-
-        res.status(200).json({
-          success:true,
-          message:"Post Edited"
-        })
-
-      }else{
-        post.comments.push({
-          user:req.user._id,
-          comment:req.body.comment,
-        })
-
+    post.comments.forEach((item, index) => {
+      if (item.user.toString() === req.user._id.toString()) {
+        commentIndex = index;
       }
+    })
+
+    if (commentIndex !== -1) {
+      post.comments[commentIndex].comment = req.body.comment
+
       await post.save();
 
       res.status(200).json({
-        success:true,
-        message:"Post Saved"
+        success: true,
+        message: "Post Edited"
       })
-     
 
-    }catch(e)
-    {
-      res.status(500).json({
-        success:false,
-        error:e.message
+    } else {
+      post.comments.push({
+        user: req.user._id,
+        comment: req.body.comment,
+      })
+
+    }
+    await post.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Post Saved"
+    })
+
+
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: e.message
+    })
+  }
+}
+
+exports.deleteComment = async (req, res) => {
+  try {
+
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(400).json({
+        success: false,
+        message: "Post not found"
       })
     }
-  }
 
-  exports.deleteComment = async (req,res) => {
-    try{
+    if (post.owner.toString() === req.user._id.toString()) {
 
-      const post = await Post.findById(req.params.id)
-
-      if(!post){
+      if (req.body.commentId == undefined) {
         return res.status(400).json({
-          success:false,
-          message:"Post not found"
+          success: false,
+          message: "Comment id is invalid"
         })
       }
 
-      if(post.owner.toString() === req.user._id.toString()){
-
-        if(req.body.commentId == undefined){
-          return res.status(400).json({
-            success:false,
-            message:"Comment id is invalid"
-          })
+      post.comments.forEach((item, index) => {
+        if (item._id.toString() === req.body.commentId.toString()) {
+          return post.comments.splice(index, 1);
         }
+      })
 
-        post.comments.forEach((item,index) => {
-          if(item._id.toString() === req.body.commentId.toString())
-          {
-            return post.comments.splice(index,1);
-          }
-        })
+      await post.save();
 
-        await post.save();
-
-        res.status(200).json({
-          success:true,
-          message:"selected Comment has deleted"
-        })
-      }else{
-        post.comments.forEach((item,index) => {
-          if(item.user.toString() === req.user._id.toString())
-          {
-            return  post.comments.splice(index,1)
-          } 
-        })
-        await post.save();
-        res.status(200).json({
-          success:true,
-          message:"your Comment has Deleted"
-        })
-      }
-
-    }catch(e)
-    {
-      res.status(500).json({
-        success:true,
-        error:e.message
+      res.status(200).json({
+        success: true,
+        message: "selected Comment has deleted"
+      })
+    } else {
+      post.comments.forEach((item, index) => {
+        if (item.user.toString() === req.user._id.toString()) {
+          return post.comments.splice(index, 1)
+        }
+      })
+      await post.save();
+      res.status(200).json({
+        success: true,
+        message: "your Comment has Deleted"
       })
     }
+
+  } catch (e) {
+    res.status(500).json({
+      success: true,
+      error: e.message
+    })
   }
+}
