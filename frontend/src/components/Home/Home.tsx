315195829c1e3cd0505e8675ce3d1639628fd383
Rajat Nai <rajatnai49@gmail.com>
2023-08-15 21:26:22 +0530

    Feed switch

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Menu from "../Menu/Menu";
 import Footer from "../Footer/Footer";
+import Feed from "../Feed/Feed";
 
 function Home() {
   return (
     <>
       <Menu />
+      <Feed />
       <Footer />
     </>
   );
