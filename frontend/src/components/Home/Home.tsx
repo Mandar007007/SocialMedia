09d3f11863fa60f952d5cba0afea787cbe6ec72b
Mandar007007 <teamm2d07@gmail.com>
2023-08-17 09:32:19 +0530

    logout navigation fix

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,11 +2,14 @@ import Menu from "../Menu/Menu";
 import Footer from "../Footer/Footer";
 import Feed from "../Feed/Feed";
 import { Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 function Home() {
+  const {user} = useSelector((state:RootState) => state.user)
   return (
     <>
-      <Menu />
+      {user && <Menu />}
       <Feed />
       <Footer />
     </>
